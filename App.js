b3ca70e07ba8d078974f5b0d78152cc9d41ad799
unknown <yfarhod75@gmail.com>
2021-05-09 01:03:08 +0200

    removing item added

--- App.js
+++ App.js
@@ -11,13 +11,19 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle }]);
   }
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId)
+    })
+  }
+
   return (
     <View style={styles.screen}>
       <GoalInput goalHandler={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => <GoalItem onDelete={() => console.log('it is working')} title={itemData.item.value} />}
+        renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
       />
     </View>
   );
